c8b6580e8277ee6d81bee5605f14a27323e08b94
yuimoz <114317681+yuimoz@users.noreply.github.com>
2024-05-01 17:57:59 -0700

    finished question 18

--- expose/javascript/part2-question18.js
+++ expose/javascript/part2-question18.js
@@ -1,1 +1,8 @@
+// modify so that the program prints out current time every second 
+function printTime() {
+   let d = new Date();
+   let time = d.toLocaleTimeString();
+   console.log(time);
+}
 
+setInterval(printTime, 1000);
