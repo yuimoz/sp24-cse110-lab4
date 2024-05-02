e57deacbd0cac9ca211a48666e0be7a2654978c8
yuimoz <114317681+yuimoz@users.noreply.github.com>
2024-05-01 17:47:11 -0700

    finished question16

--- expose/javascript/part2-question16.js
+++ expose/javascript/part2-question16.js
@@ -1,1 +1,19 @@
+let statistics = {
+   redCars: 21,
+   blueCars: 45, 
+   greenCars: 12,
+   raceCars: 5, 
+   blackCars: 40,
+   rareCars: 2
+};
 
+for (const property in statistics) {
+   // print out the value of the property if:
+   // property starts with r
+   // or if the value is an odd num
+
+   if (property.startsWith('r') || statistics[property] % 2 != 0) {
+      console.log(statistics[property]);
+   }
+   
+}
